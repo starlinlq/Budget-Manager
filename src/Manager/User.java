0b69ba8fc1f95c027efb6c521f6b7b714cc321e6
starlin <starlinlq@gmail.com>
2021-11-08 13:45:55 -0600

    User business logic

--- src/Manager/User.java
+++ src/Manager/User.java
@@ -6,6 +6,7 @@ public class User {
     private String name;
     private int income;
     private int balance;
+
     HashMap<String, HashMap<String, Integer>> list = new HashMap<>();
 
     User(String name, int income, int balance){
@@ -14,5 +15,37 @@ public class User {
         this.balance= balance;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public void addExpense(String key, String name, int price){
+        var expense = this.list.get(key);
+
+        if(expense.containsKey(name)){
+            expense.put(name, expense.get(name) + price);
+        } else
+            this.list.get(key).put(name, price);
+    }
+
+    public HashMap<String, Integer> getExpenses(String key) {
+        return this.list.get(key);
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getIncome() {
+        return income;
+    }
 }
