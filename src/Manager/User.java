5ed66b58456c30484dbeb256220a53c158abae80
Anthony <amcmullen9291@gmail.com>
2021-11-08 15:40:51 -0500

    clear scene before switch statment

--- src/Manager/User.java
+++ src/Manager/User.java
@@ -3,9 +3,11 @@ package Manager;
 import java.util.HashMap;
 
 public class User {
-    private String name = "tom";
-    private int income = 0;
-    private int balance = 0;
+    private String name;
+    private int income;
+    private int balance;
+    private int expensesTotal;
+
     HashMap<String, HashMap<String, Integer>> list = new HashMap<>();
 
     User(String name, int income, int balance){
@@ -14,5 +16,40 @@ public class User {
         this.balance= balance;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public void addExpense(String key, String name, int price){
+        var expense = this.list.get(key);
+
+        if(expense.containsKey(name)){
+            expense.put(name, expense.get(name) + price);
+        } else
+            this.list.get(key).put(name, price);
+
+    }
+
+    public HashMap<String, Integer> getExpenses(String key) {
+        return this.list.get(key);
+    }
+
+    public int getBalance() {
+
+        return balance;
+    }
+
+    public int getIncome() {
+        return income;
+    }
 }
+
