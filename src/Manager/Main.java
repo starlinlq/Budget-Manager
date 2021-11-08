da656bea9262b704adae555b9883abe8a2e91b97
starlin <starlinlq@gmail.com>
2021-11-08 11:50:28 -0600

    todos

--- src/Manager/Main.java
+++ src/Manager/Main.java
@@ -2,10 +2,26 @@ package Manager;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello");
-        System.out.println("Console");
+    //purchases.txt
+    //User
+    //userCollection - is going to save all the users created -
+    //      //     / - save in the file
+    //hashmap key-values (name (key), price (value)) for the categories, and users
+    //
 
+
+    public static void main(String[] args) {
+        displayMenu();
+    }
+
+    public static void displayMenu(){
+        System.out.println("Choose your action: \n" +
+                           "1) Add income \n" +
+                           "2) Add Purchase \n" +
+                           "3) Show list of purchases \n" +
+                           "4) Balance \n" +
+                           "5) Save \n" +
+                           "6) Load  \n" +
+                           "0) Exit ");
     }
 }
